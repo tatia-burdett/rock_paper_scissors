c57eda821827f2b4da92e775342ed4f95c45ad51
Tatjana Burdett <ttjncarney@gmail.com>
2020-10-16 09:43:05 -0600

    pseudocode written

---
+++ script.js
@@ -0,0 +1,50 @@
+/* 
+User prompted to make a choice: Rock, Paper, Scissors.
+User choice recorded in a variable.
+Computer makes a random choice.
+Computer choice record in a variable.
+
+If user choice is Rock
+	and computer choice is Scissors
+		+1 point for user
+		"You win this round"
+	and computer choice is Paper
+		+1 point for computer
+		"You lose this round"
+	and computer choice is Rock
+		0 points given
+		"It's a tie"
+
+If user choice is Scissors
+	and computer choice is Paper
+		+1 point for user
+		"You win this round"
+	and computer choice is Rock
+		+1 point for computer
+		"You lose this round"
+	and computer choice is Scissors
+		0 points given
+		"It's a tie"
+
+If user choice is Paper
+	and computer choice is Rock
+		+1 point for user
+		"You win this round"
+	and computer choice is Scissors
+		+1 point for computer
+		"You lose this round"
+	and computer choice is Paper
+		0 points given
+		"It's a tie"
+
+If the rounds played is less than 5
+		play another round
+	Else... if rounds played = 5
+		Display total user points
+		Display total computer points
+
+If user points > computer points
+		"Congratulations, you win!"
+	Else...
+		"Sorry, you lose!"
+*/
