46a94b7e928d0988cf5b2a0dad03675af51538a5
Tatjana Burdett <ttjncarney@gmail.com>
2020-10-16 12:33:45 -0600

    game() function to calculate winner / loser and total points

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let choices = ['rock', 'paper', 'scissors'];
 let userPoints = 0;
 let compPoints = 0;
+let round = 0;
 
 function playRound(playerSelection, computerSelection) {
 	// User prompted to make a choice: Rock, Paper, Scissors and user choice recorded in a variable.
@@ -18,40 +19,48 @@ function playRound(playerSelection, computerSelection) {
 	// If user choice ____ and computer choice ____, result ____
 	if (playerSelection === 'rock' && computerSelection === 'scissors') {
 		userPoints = userPoints + 1;
-		console.log(`You win this round`);
+		round = round + 1;
+		console.log(`You win this round, you have ${userPoints} and the computer has ${compPoints}. Round ${round}/5`);
 	} else if (playerSelection === 'rock' && computerSelection === 'paper') {
 		compPoints = compPoints + 1;
-		console.log(`You lose this round`);
+		round = round + 1;
+		console.log(`You lose this round, you have ${userPoints} and the computer has ${compPoints}. Round ${round}/5`);
 	} else if (playerSelection === 'rock' && computerSelection === 'rock') {
-		console.log(`It is a tie!`)
+		round = round + 1;
+		console.log(`It is a tie, you have ${userPoints} and the computer has ${compPoints}. Round ${round}/5`)
 	} else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-		userPoints = userPoints + 1
-		console.log(`You win this round`);
+		userPoints = userPoints + 1;
+		round = round + 1;
+		console.log(`You win this round, you have ${userPoints} and the computer has ${compPoints}. Round ${round}/5`);
 	} else if (playerSelection === 'scissors' && computerSelection === 'rock') {
 		compPoints = compPoints + 1;
-		console.log(`You lose this round`);
+		round = round + 1;
+		console.log(`You lose this round, you have ${userPoints} and the computer has ${compPoints}. Round ${round}/5`);
 	} else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
-		console.log(`It is a tie!`);
+		round = round + 1;
+		console.log(`It is a tie, you have ${userPoints} and the computer has ${compPoints}. Round ${round}/5`);
 	} else if (playerSelection === 'paper' && computerSelection === 'rock') {
-		userPoints = userPoints + 1
-		console.log(`You win this round`);
+		userPoints = userPoints + 1;
+		round = round + 1;
+		console.log(`You win this round, you have ${userPoints} and the computer has ${compPoints}. Round ${round}/5`);
 	} else if (playerSelection == 'paper' && computerSelection == 'scissors') {
 		compPoints = compPoints + 1;
-		console.log(`You lose this round`);
+		round = round + 1;
+		console.log(`You lose this round, you have ${userPoints} and the computer has ${compPoints}. Round ${round}/5`);
 	} else {
-		console.log(`It is a tie!`);
+		round = round + 1;
+		console.log(`It is a tie, you have ${userPoints} and the computer has ${compPoints}. Round ${round}/5`);
 	};
 };
 
 
-
-/* If the rounds played is less than 5
- 		play another round
- 	Else... if rounds played = 5
- 		Display total user points
- 		Display total computer points */
-
-/* If user points > computer points
- 		"Congratulations, you win!"
- 	Else...
- 		"Sorry, you lose!" */
+// Round is played 5 times. Once played 5 times, output user and computer points, declare winner.
+function game() {
+	if (round === 5 && userPoints > compPoints) {
+		console.log(`Congratulations! You win! You have ${userPoints} points and the computer has ${compPoints} points!`)
+	} else if (round === 5 && userPoints < compPoints) {
+		console.log(`You lose this time! You have ${userPoints} points and the computer has ${compPoints} points.`)
+	} else {
+		console.log(`Keep playing, you have played ${round} of 5 rounds.`)
+	}
+}
